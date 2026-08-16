b8f761e862fb0db82fdd437586d532c19c16db1d
Sridattu Reddy <sridattureddypiduru@gmail.com>
2026-08-16 23:02:37 +0530

    SnowFlake Src Codes

---
+++ Day-21-24/Getting-Started.sql
@@ -0,0 +1,12 @@
+USE WAREHOUSE PRACTISE_WH;
+
+
+
+use database snowflake_learning_db;
+
+show schemas;
+select top 10* from information_schema.databases;
+
+-- Soo Just Create a WH with the Specification as your type and use sql querries to explore...
+
+--Create a New Data-Base that we will work on 
